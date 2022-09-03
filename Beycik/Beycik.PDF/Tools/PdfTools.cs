9553cdbc2d1b2ddb9880df9e89217be83b52f920
Hubertus Sack <hubertussack@outlook.com>
2022-09-03 03:08:59 +0200

    Adding title and author cleanup

---
+++ Beycik/Beycik.PDF/Tools/PdfTools.cs
@@ -0,0 +1,12 @@
+﻿namespace Beycik.PDF.Tools
+{
+    internal static class PdfExt
+    {
+        public static string CleanText(string text)
+        {
+            return text.Replace("\\", "\\\\")
+                .Replace("(", "\\(")
+                .Replace(")", "\\)");
+        }
+    }
+}
--- Beycik/Beycik.PDF/Xml2Pdf.cs
+++ Beycik/Beycik.PDF/Xml2Pdf.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Beycik.Model.Roots;
 using Beycik.PDF.Config;
+using Beycik.Draw.Fonts.API;
+using static Beycik.PDF.Tools.PdfExt;
 
 namespace Beycik.PDF
 {
@@ -17,15 +19,19 @@ namespace Beycik.PDF
         {
         }
 
-        public void Save(string filePath, PdfOptions options, IConfig config)
+        private static void FillConfig(IConfig config, XmlDoc model)
         {
+            config.Title ??= model.FormInfo.FormServer.FormName;
+            config.Title = CleanText(config.Title);
+            config.Author ??= model.FormInfo.FormServer.Copyright;
+            config.Author = CleanText(config.Author);
+        }
 
-
-
-
-
-
-
+        public void Save(string pdfFile, PdfOptions options,
+            IConfig cfg = null, IFontManager fm = null)
+        {
+            var config = cfg ?? new PdfConfig();
+            FillConfig(config, _doc);
 
             throw new NotImplementedException();
         }
