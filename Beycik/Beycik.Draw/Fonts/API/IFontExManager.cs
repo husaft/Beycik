b2dd6c7d06fd01d76f60453bb35e1c97a1b48074
Hubertus Sack <hubertussack@outlook.com>
2022-09-12 21:35:12 +0200

    Extended font manager to be more flexible

---
+++ Beycik/Beycik.Draw/Fonts/API/IFontExManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SixLabors.Fonts;
+
+namespace Beycik.Draw.Fonts.API
+{
+    public interface IFontExManager : IFontManager
+    {
+        IDictionary<string, string> Mapping { get; }
+
+        FontCollection Fonts { get; }
+    }
+}
--- Beycik/Beycik.Draw/Fonts/DefaultFontManager.cs
+++ Beycik/Beycik.Draw/Fonts/DefaultFontManager.cs
@@ -4,28 +4,34 @@ using SixLabors.Fonts;
 
 namespace Beycik.Draw.Fonts
 {
-    internal class DefaultFontManager : IFontManager
+    internal class DefaultFontManager : IFontExManager
     {
+        private readonly FontCollection _collection;
         private readonly IDictionary<string, string> _mapping;
 
-        public DefaultFontManager()
+        public DefaultFontManager(bool addSystemFonts = true)
         {
+            _collection = new FontCollection();
             _mapping = new Dictionary<string, string>
             {
                 { "Helvetica", "Arial" },
                 { "Courier", "Courier New" }
             };
+            if (addSystemFonts) _collection.AddSystemFonts();
         }
+
+        IDictionary<string, string> IFontExManager.Mapping => _mapping;
+        FontCollection IFontExManager.Fonts => _collection;
 
         public IFont Create(FontDescriptor desc)
         {
             var fontName = desc.Face;
             var fontSize = desc.Size;
             var fontStyle = desc.Style.Convert();
-            if (!SystemFonts.TryGet(fontName, out _) &&
+            if (!_collection.TryGet(fontName, out _) &&
                 _mapping.TryGetValue(fontName, out var newFontName))
                 fontName = newFontName;
-            var font = SystemFonts.CreateFont(fontName, fontSize, fontStyle);
+            var font = _collection.Get(fontName).CreateFont(fontSize, fontStyle);
             return new DefaultFont(font);
         }
     }
